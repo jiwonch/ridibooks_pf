cd91b3b6990c26c44948afd5dfcc0aed6fc4f84d
JIWON CHOI <rizjw26@gmail.com>
2022-07-22 16:08:31 +0900

    20220722_slideslide

--- js/slide.js
+++ js/slide.js
@@ -1,22 +1,37 @@
 $(document).ready(function () {
-    $(".slideIB").not(":first").css({ "flex-grow": "none", "flex-basis": "none" });
+    $(".slideIB").not(":first").css({ "flex-grow": "0", "flex-basis": "46" });
 
 
 
 });
 
 function prev() {
-    $(".slideIB:last").prependTo("#slideImgBox").css({ "flex-grow": "none", "flex-basis": "none" });
+    $(".slideIB:last").prependTo("#slideImgBox").css({ "flex-grow": "0", "flex-basis": "46px" });
     $(".slideIB:first").stop().animate({ "flex-grow": "1", "flex-basis": "0" }, "slow", "linear");
-    $(".slideIB").eq(1).stop().animate({ "flex-grow": "0", "flex-basis": "40px" }, "slow", "linear");
-    $(".slideIB").eq(2).stop().animate({ "flex-grow": "0", "flex-basis": "40px" }, "slow", "linear");
+    $(".slideIB").eq(2).stop().animate({ "flex-grow": "0", "flex-basis": "46px" }, "slow", "linear");
+    $(".slideIB").eq(1).stop().animate({ "flex-grow": "0", "flex-basis": "46px" }, "slow", "linear");
+    $(".slideIB").eq(3).css({ "flex-grow": "0", "flex-basis": "0" });
+    $(".slideIB").eq(4).css({ "flex-grow": "0", "flex-basis": "0" });
+    $(".slideIB").eq(5).css({ "flex-grow": "0", "flex-basis": "0" });
+    $(".slideIB").eq(6).css({ "flex-grow": "0", "flex-basis": "0" });
+    $(".slideIB").eq(7).css({ "flex-grow": "0", "flex-basis": "0" });
+    $(".slideIB").eq(8).css({ "flex-grow": "0", "flex-basis": "0" });
+    $(".slideIB").eq(9).css({ "flex-grow": "0", "flex-basis": "0" });
 }
 
 function next() {
-    $(".slideIB:first").stop().animate({ "flex-grow": "none", "flex-basis": "none" }, "slow", "linear");
+    $(".slideIB:first").stop().animate({ "flex-grow": "0", "flex-basis": "0" }, "slow", "linear");
     $(".slideIB").eq(1).stop().animate({ "flex-grow": "1", "flex-basis": "0" }, "slow", "linear", function () {
         $(".slideIB:first").appendTo("#slideImgBox");
-        $(".slideIB:last").css({ "flex-grow": "0", "flex-basis": "40px" });
+        $(".slideIB").eq(2).css({ "flex-grow": "0", "flex-basis": "46px" });
+        $(".slideIB").eq(1).css({ "flex-grow": "0", "flex-basis": "46px" });
+        $(".slideIB").eq(3).css({ "flex-grow": "0", "flex-basis": "0" });
+        $(".slideIB").eq(4).css({ "flex-grow": "0", "flex-basis": "0" });
+        $(".slideIB").eq(5).css({ "flex-grow": "0", "flex-basis": "0" });
+        $(".slideIB").eq(6).css({ "flex-grow": "0", "flex-basis": "0" });
+        $(".slideIB").eq(7).css({ "flex-grow": "0", "flex-basis": "0" });
+        $(".slideIB").eq(8).css({ "flex-grow": "0", "flex-basis": "0" });
+        $(".slideIB").eq(9).css({ "flex-grow": "0", "flex-basis": "0" });
     });
 }
 
