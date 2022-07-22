677f7607f6fb508f4114b871970919370c59ba9d
JIWON CHOI <rizjw26@gmail.com>
2022-07-22 15:25:52 +0900

    20220722_slide
    잘못 구현된 거 수정해야 함.

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,31 @@
-$(document).ready(function(){
-    
-    function prev(){
-        
-    }
+$(document).ready(function () {
+    $(".slideIB").not(":first").css({ "flex-grow": "none", "flex-basis": "none" });
+
+
+
+});
+
+function prev() {
+    $(".slideIB:last").prependTo("#slideImgBox").css({ "flex-grow": "none", "flex-basis": "none" });
+    $(".slideIB:first").stop().animate({ "flex-grow": "1", "flex-basis": "0" }, "slow", "linear");
+    $(".slideIB").eq(1).stop().animate({ "flex-grow": "0", "flex-basis": "40px" }, "slow", "linear");
+    $(".slideIB").eq(2).stop().animate({ "flex-grow": "0", "flex-basis": "40px" }, "slow", "linear");
+}
+
+function next() {
+    $(".slideIB:first").stop().animate({ "flex-grow": "none", "flex-basis": "none" }, "slow", "linear");
+    $(".slideIB").eq(1).stop().animate({ "flex-grow": "1", "flex-basis": "0" }, "slow", "linear", function () {
+        $(".slideIB:first").appendTo("#slideImgBox");
+        $(".slideIB:last").css({ "flex-grow": "0", "flex-basis": "40px" });
+    });
+}
+
+setInterval(next, 5000);
+
+$(".slideprev").on("click", function () {
+    prev();
+});
+
+$(".slidenext").on("click", function () {
+    next();
 });
