724ea9cd00c7233564ef4aee45cbd47043d459b6
JIWON CHOI <rizjw26@gmail.com>
2022-07-25 17:59:41 +0900

    20220725_sub_review

--- js/book.js
+++ js/book.js
@@ -9,3 +9,137 @@ $.ajax({
         $(".img_box").append("<img src ='" + msg.documents[0].thumbnail + "'/>");
 
     });
+
+    // review write star red color
+    $(document).ready(function(){
+        $(".star_1").mouseover(function(){
+            $(".star_1").addClass('color_red');
+        });
+
+        $(".star_1").mouseout(function(){
+            $(".star_1").removeClass('color_red');
+        });
+
+        $(".star_2").mouseover(function(){
+            $(".star_1").addClass('color_red');
+            $(".star_2").addClass('color_red');
+        });
+
+        $(".star_2").mouseout(function(){
+            $(".star_1").removeClass('color_red');
+            $(".star_2").removeClass('color_red');
+        });
+
+        $(".star_3").mouseover(function(){
+            $(".star_1").addClass('color_red');
+            $(".star_2").addClass('color_red');
+            $(".star_3").addClass('color_red');
+        });
+
+        $(".star_3").mouseout(function(){
+            $(".star_1").removeClass('color_red');
+            $(".star_2").removeClass('color_red');
+            $(".star_3").removeClass('color_red');
+        });
+
+        $(".star_4").mouseover(function(){
+            $(".star_1").addClass('color_red');
+            $(".star_2").addClass('color_red');
+            $(".star_3").addClass('color_red');
+            $(".star_4").addClass('color_red');
+        });
+
+        $(".star_4").mouseout(function(){
+            $(".star_1").removeClass('color_red');
+            $(".star_2").removeClass('color_red');
+            $(".star_3").removeClass('color_red');
+            $(".star_4").removeClass('color_red');
+        });
+
+        $(".star_5").mouseover(function(){
+            $(".star_1").addClass('color_red');
+            $(".star_2").addClass('color_red');
+            $(".star_3").addClass('color_red');
+            $(".star_4").addClass('color_red');
+            $(".star_5").addClass('color_red');
+        });
+
+        $(".star_5").mouseout(function(){
+            $(".star_1").removeClass('color_red');
+            $(".star_2").removeClass('color_red');
+            $(".star_3").removeClass('color_red');
+            $(".star_4").removeClass('color_red');
+            $(".star_5").removeClass('color_red');
+        });
+    });
+
+    // review notice slide toggle
+    $(document).ready(function(){
+        $("#noticeBox").hide();
+        $('.review_notice').click(function(){
+            $("#noticeBox").slideToggle();
+        });
+    });
+
+    //review confirm
+    $(document).ready(function(){
+        $("#text_review").click(function(){
+            confirm("로그인 이후 이용 가능합니다. 로그인을 하시겠습니까?")
+        });
+
+        $(".star").click(function(){
+            confirm("로그인 이후 이용 가능합니다. 로그인을 하시겠습니까?")
+        });
+    });
+
+    //review nav
+    $(document).ready(function(){
+        $('.pn1_1').addClass('hover_line');
+        $('.pn2_1').addClass('hover_black');
+
+        $(".pn1_2").click(function(){
+            $(".pn2_hide").hide();
+            $(this).addClass('hover_line');
+            $('.pn1_1').removeClass('hover_line');
+            $('.pn2_1').addClass('hover_black');
+            $(".pn2_2").removeClass('hover_black');
+        });
+
+        $(".pn1_1").click(function(){
+            $(".pn2_hide").show();
+            $(this).addClass('hover_line');
+            $('.pn1_2').removeClass('hover_line');
+            $('.pn2_1').addClass('hover_black');
+            $(".pn2_2").removeClass('hover_black');
+            $(".pn2_3").removeClass('hover_black');
+            $(".pn2_4").removeClass('hover_black');
+        });
+
+        $(".pn2_1").click(function(){
+            $(this).addClass('hover_black');
+            $(".pn2_2").removeClass('hover_black');
+            $(".pn2_3").removeClass('hover_black');
+            $(".pn2_4").removeClass('hover_black');
+        });
+
+        $(".pn2_2").click(function(){
+            $(this).addClass('hover_black');
+            $(".pn2_1").removeClass('hover_black');
+            $(".pn2_3").removeClass('hover_black');
+            $(".pn2_4").removeClass('hover_black');
+        });
+
+        $(".pn2_3").click(function(){
+            $(this).addClass('hover_black');
+            $(".pn2_1").removeClass('hover_black');
+            $(".pn2_2").removeClass('hover_black');
+            $(".pn2_4").removeClass('hover_black');
+        });
+
+        $(".pn2_4").click(function(){
+            $(this).addClass('hover_black');
+            $(".pn2_1").removeClass('hover_black');
+            $(".pn2_2").removeClass('hover_black');
+            $(".pn2_3").removeClass('hover_black');
+        });
+    });
