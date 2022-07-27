d1ce8e5ad16b61978579a9d1a55d8ee6826ad5bf
JIWON CHOI <rizjw26@gmail.com>
2022-07-27 13:20:08 +0900

    20220727_대표저서 리뷰 말풍선, 별점
    하단 ajax

--- js/book.js
+++ js/book.js
@@ -13,40 +13,82 @@ $.ajax({
 // review write star red color
 $(document).ready(function () {
     $(".star_1").mouseover(function () {
+        $(".ev_write p").hide();
         $(".star_1").addClass('color_red');
+        $(".write_1").show();
+        $(".write_2").hide();
+        $(".write_3").hide();
+        $(".write_4").hide();
+        $(".write_5").hide();
     });
 
     $(".star_1").mouseout(function () {
         $(".star_1").removeClass('color_red');
+        $(".ev_write p").show();
+        $(".write_1").hide();
+        $(".write_2").hide();
+        $(".write_3").hide();
+        $(".write_4").hide();
+        $(".write_5").hide();
     });
 
     $(".star_2").mouseover(function () {
+        $(".ev_write p").hide();
         $(".star_1").addClass('color_red');
         $(".star_2").addClass('color_red');
+        $(".write_1").hide();
+        $(".write_2").show();
+        $(".write_3").hide();
+        $(".write_4").hide();
+        $(".write_5").hide();
     });
 
     $(".star_2").mouseout(function () {
         $(".star_1").removeClass('color_red');
         $(".star_2").removeClass('color_red');
+        $(".ev_write p").show();
+        $(".write_1").hide();
+        $(".write_2").hide();
+        $(".write_3").hide();
+        $(".write_4").hide();
+        $(".write_5").hide();
     });
 
     $(".star_3").mouseover(function () {
+        $(".ev_write p").hide();
         $(".star_1").addClass('color_red');
         $(".star_2").addClass('color_red');
         $(".star_3").addClass('color_red');
+        $(".write_1").hide();
+        $(".write_2").hide();
+        $(".write_3").show();
+        $(".write_4").hide();
+        $(".write_5").hide();
     });
 
     $(".star_3").mouseout(function () {
         $(".star_1").removeClass('color_red');
         $(".star_2").removeClass('color_red');
         $(".star_3").removeClass('color_red');
+        $(".ev_write p").show();
+        $(".write_1").hide();
+        $(".write_2").hide();
+        $(".write_3").hide();
+        $(".write_4").hide();
+        $(".write_5").hide();
     });
 
     $(".star_4").mouseover(function () {
+        $(".ev_write p").hide();
         $(".star_1").addClass('color_red');
         $(".star_2").addClass('color_red');
         $(".star_3").addClass('color_red');
         $(".star_4").addClass('color_red');
+        $(".write_1").hide();
+        $(".write_2").hide();
+        $(".write_3").hide();
+        $(".write_4").show();
+        $(".write_5").hide();
     });
 
     $(".star_4").mouseout(function () {
@@ -54,14 +96,26 @@ $(document).ready(function () {
         $(".star_2").removeClass('color_red');
         $(".star_3").removeClass('color_red');
         $(".star_4").removeClass('color_red');
+        $(".ev_write p").show();
+        $(".write_1").hide();
+        $(".write_2").hide();
+        $(".write_3").hide();
+        $(".write_4").hide();
+        $(".write_5").hide();
     });
 
     $(".star_5").mouseover(function () {
+        $(".ev_write p").hide();
         $(".star_1").addClass('color_red');
         $(".star_2").addClass('color_red');
         $(".star_3").addClass('color_red');
         $(".star_4").addClass('color_red');
         $(".star_5").addClass('color_red');
+        $(".write_1").hide();
+        $(".write_2").hide();
+        $(".write_3").hide();
+        $(".write_4").hide();
+        $(".write_5").show();
     });
 
     $(".star_5").mouseout(function () {
@@ -70,6 +124,12 @@ $(document).ready(function () {
         $(".star_3").removeClass('color_red');
         $(".star_4").removeClass('color_red');
         $(".star_5").removeClass('color_red');
+        $(".ev_write p").show();
+        $(".write_1").hide();
+        $(".write_2").hide();
+        $(".write_3").hide();
+        $(".write_4").hide();
+        $(".write_5").hide();
     });
 });
 
@@ -169,3 +229,40 @@ $(document).ready(function(){
     });
 });
 
+// presntation 대표 저서 인기순 최신순 평점순 클릭 이벤트
+$(document).ready(function(){
+    $(".presenli_1").addClass('presen_black');
+
+    $(".presenli_1").click(function(){
+        $(this).addClass('presen_black')
+        $(".presenli_2").removeClass('presen_black');
+        $(".presenli_3").removeClass('presen_black');
+    });
+
+    $(".presenli_2").click(function(){
+        $(this).addClass('presen_black')
+        $(".presenli_1").removeClass('presen_black');
+        $(".presenli_3").removeClass('presen_black');
+    });
+
+    $(".presenli_3").click(function(){
+        $(this).addClass('presen_black')
+        $(".presenli_1").removeClass('presen_black');
+        $(".presenli_2").removeClass('presen_black');
+    });
+});
+
+// 대표 저서 ajax로 가져오기
+$.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: { query: "황보름", size:2 },
+    headers: { Authorization: "KakaoAK 9b457f655c7841e82fec1c92bda0e61a" }
+})
+    .done(function (msg) {
+
+        for (var i = 0; i < 3; i++) {
+            $(".presen_Img a").eq(i).append("<img src ='" + msg.documents[i].thumbnail + "'/>");
+            $(".presenbook_Box h4 a").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+        }
+    });
